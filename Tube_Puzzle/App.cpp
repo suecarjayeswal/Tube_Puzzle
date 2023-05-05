0d458b8377eb34519657d0218094f61ef746849c
Suecar Jayeswal <suecarjayeswal098@gmail.com>
2023-05-05 23:16:40 +0545

    somehow it works with few red columns and undo button

--- Tube_Puzzle/App.cpp
+++ Tube_Puzzle/App.cpp
@@ -1,12 +1,30 @@
+
 #include "App.h"
 #include "MainFrame.h"
 #include <wx/wx.h>
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit()
+    {
+        MainFrame* frame = new MainFrame(wxT("Tube Puzzle Game"));
+        frame->Show(true);
+        return true;
+    }
+};
 
-wxIMPLEMENT_APP(App);
-bool App::OnInit() {
-	MainFrame* mainFrame = new MainFrame(" C++ GUI");
-	mainFrame->SetClientSize(800, 600);
-	mainFrame->Center();
-	mainFrame->Show();
-	return true;
-}
+wxIMPLEMENT_APP(MyApp);
+
+
+//#include "App.h"
+//#include "MainFrame.h"
+//#include <wx/wx.h>
+//
+//wxIMPLEMENT_APP(App);
+//bool App::OnInit() {
+//	MainFrame* mainFrame = new MainFrame(" C++ GUI");
+//	mainFrame->SetClientSize(800, 600);
+//	mainFrame->Center();
+//	mainFrame->Show();
+//	return true;
+//}
--- Tube_Puzzle/MainFrame.h
+++ Tube_Puzzle/MainFrame.h
@@ -1,11 +1,31 @@
+
 #pragma once
+
 #include <wx/wx.h>
 
-class MainFrame: public wxFrame
+class MainFrame : public wxFrame
 {
 public:
-	MainFrame(const wxString& title);
+    MainFrame(const wxString& title);
 
+private:
+    void OnUndo(wxCommandEvent& event);
+
+    wxDECLARE_EVENT_TABLE();
 };
 
+enum
+{
+    ID_UNDO = wxID_HIGHEST + 1
+};
+
+//#pragma once
+//#include <wx/wx.h>
+//
+//class mainframe: public wxframe
+//{
+//public:
+//	mainframe(const wxstring& title);
+//
+//};
 
